b29914c8cd72cf5eef4ba3a4840b40a8a825066d
magnum-z <zr260578jea@gmail.com>
2017-07-27 23:14:53 +0300

    Added Task 1

---
+++ Lesson_03_Task_1/Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lesson_03_Task_1.Models;
+
+namespace Lesson_03_Task_1.Controllers
+{
+    public class ProductController : Controller
+    {        
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateProducts()
+        {
+            List<Product> products = new List<Product>();
+
+            products.Add(new Product()
+            {
+                ID = 1,
+                Name = "product 1",
+                Price = 11.42m,
+                CreateDate = DateTime.Now.ToShortDateString()
+            });
+
+            products.Add(new Product()
+            {
+                ID = 2,
+                Name = "product 2",
+                Price = 42.20m,
+                CreateDate = DateTime.Now.ToShortDateString()
+            });
+
+            products.Add(new Product()
+            {
+                ID = 3,
+                Name = "product 3",
+                Price = 64.95m,
+                CreateDate = DateTime.Now.ToShortDateString()
+            });
+
+            return View(products);
+        }
+    }
+}
